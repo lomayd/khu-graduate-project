7d5808ae555e65ac4b4582f80c93c074d5b5b788
lomayd <jinho10167@gmail.com>
2023-10-19 23:32:21 +0900

    feat: Project Initialize

---
+++ src/main/java/lomayd/khugraduateproject/api/KhuGraduateProjectApplication.java
@@ -0,0 +1,13 @@
+package lomayd.khugraduateproject.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhuGraduateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhuGraduateProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/khugraduateproject/KhuGraduateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.khugraduateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhuGraduateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
